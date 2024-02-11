7f0d3b29b07224aad2cf17e4a694846daa637818
Dan <danielarturomt@gmail.com>
2024-02-11 18:42:29 +0000

    feat: practical lesson #1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NxWelcomeComponent } from './nx-welcome.component';
+import { BobTestComponent } from './bob.component';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [NxWelcomeComponent, RouterModule, BobTestComponent],
   selector: 'bob-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
